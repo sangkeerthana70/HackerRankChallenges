970b7b34069ba87797ff54b6ee6e832b4dd16973
Anuradha <sangkeerthana@gmail.com>
2018-03-01 03:32:41 +0000

    Added Day2.js

---
+++ Day2.js
@@ -0,0 +1,78 @@
+/*Objective 
+In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+Input Format
+
+There are  lines of numeric input: 
+The first line has a double,  (the cost of the meal before tax and tip). 
+The second line has an integer,  (the percentage of  being added as tip). 
+The third line has an integer,  (the percentage of  being added as tax).
+
+Output Format
+
+Print The total meal cost is totalCost dollars., where  is the rounded integer result of the entire bill ( with added tax and tip).
+
+Sample Input
+
+12.00
+20
+8
+Sample Output
+
+The total meal cost is 15 dollars.
+
+We round  to the nearest dollar (integer) and then print our result:
+
+The total meal cost is 15 dollars.*/
+
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+	_input = _input.split(new RegExp('[\n ]+'));
+	main(+(_input[0]), +(_input[1]), +(_input[2]));    
+});
+
+function main(mealCost, tipPercent, taxPercent) {
+    process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function (data){
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+    readLine()
+}
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var meal_cost = parseFloat(readLine());//string to a decimal 
+    var tip_percent = parseInt(readLine());//string to a integer
+    var tax_percent = parseInt(readLine());//string to a integer
+    var total_cost = 0;
+    var tip = meal_cost * (tip_percent/100);
+    var tax = meal_cost * (tax_percent/100);
+    total_cost = "The total meal cost is " + Math.round(meal_cost + tip + tax) + "dollars";
+    //console.log("The total meal cost is " + total_cost + "dollars");
+}
+
+}
+
