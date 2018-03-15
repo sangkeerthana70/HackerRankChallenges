6d1c96b82196af2db59fdfc16a286c19f5d613bc
Anuradha <sangkeerthana@gmail.com>
2018-03-15 03:07:25 +0000

    Modified Day2.js operators in hackerank

--- Day2.js
+++ Day2.js
@@ -71,8 +71,7 @@ function main() {
     var tip = mealCost * (tipPercent/100);
     var tax = mealCost * (taxPercent/100);
     totalCost =  mealCost + tip + tax;
-    
+    totalCost = Math.round(totalCost);
     console.log("The total meal cost is " + totalCost + "dollars");
-    
-};
-
+}    
+}
